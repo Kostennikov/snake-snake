0c3a3129d13900d217f7b98c2a99cb259a65507f
Паша Костенников <pavel.kostennikov@gmail.com>
2020-11-12 16:08:53 +0300

    Создал прелоадер для спрайтов

--- js/game.js
+++ js/game.js
@@ -1,25 +1,51 @@
 let game = {
-	start: function (){
-		let canvas = document.getElementById('mycanvas');
-		//контекст, позволяет рисовать графику
-		let ctx = canvas.getContext('2d')
+	canvas: null,
+	ctx: null,
+	sprites: {
+		background: null,
+		cell: null,
+	},
 
-		//получение изображения для отрисовки
-		let background = new Image()
-		background.src = 'img/background.png';
-
-		background.addEventListener('load', () => {
-			//выполнение перерисовки кадра анимации (рендер графики)
-			window.requestAnimationFrame(() => {
-				//вывод изображения на экран
-				//графика запланированная для перерисовки
-				ctx.drawImage(background, 0, 0);
-			});
-		})
+	start() {
+		this.canvas = document.getElementById('mycanvas');
+		// контекст, позволяет рисовать графику
+		this.ctx = this.canvas.getContext('2d');
 
 
+		this.preload(() => {
+			this.run()
+		});
+	},
+	preload(callback){
+		let loaded = 0;
+		let required = 2;
+		let onAssetLoad = () => {
+			++loaded;
 
+			if (loaded >= required) {
+				// когда все спрайты загружены:
+				callback();
+			}
+		}
 
+		for (let key in this.sprites){
+			// получение изображения для отрисовки
+			this.sprites[key] = new Image();
+			this.sprites[key].src = 'img/' + key + '.png';
+			// запуск при полной загрузке страницы
+			this.sprites[key].addEventListener('load', onAssetLoad);
+		}
+
+	},
+	// запуск игры
+	run() {
+		// выполнение перерисовки кадра анимации (рендер графики)
+		window.requestAnimationFrame(() => {
+			// вывод изображения на экран
+			// графика запланированная для перерисовки
+			this.ctx.drawImage(this.sprites.background, 0, 0);
+			this.ctx.drawImage(this.sprites.cell, 320, 180);
+		});
 	}
 };
 
